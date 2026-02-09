a4bbe81be82513756941e1816a0f16a9e7ad88f8
AmauryRomero1285 <230190@utxicotepec.edu.mx>
2026-02-09 06:35:44 -0600

    Generate files with code
    Add a models to generate db

--- app/models/modelServices.py
+++ app/models/modelServices.py
@@ -1,18 +1,16 @@
 '''
-Docstring for models.vehiculos
+Docstring for models.servicio
 '''
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum, Date , ForeignKey
 from sqlalchemy.orm import relationship
 from config.db import Base
-class Vehiculo(Base):
-    ''' Docstring for Vehiculo'''
-    __tablename__ = "tbb_vehiculo"
+class Servicio(Base):
+    ''' Docstring for Servicio'''
+    __tablename__ = "tbc_servicio"
     Id = Column(Integer, primary_key=True, index=True)
-    cliente_Id = Column(Integer, ForeignKey("tbc_cliente.Id"))
-    matricula = Column(String(60))
-    modelo = Column(String(60))
-    color = Column(String(60))
-    numero_del_dueno = Column(String(60))
+    nombre = Column(String(60))
+    descripcion = Column(String(60))
+    costo = Column(Integer)
     estatus = Column(Boolean)
     fecha_registro = Column(DateTime)
     fecha_modificacion = Column(DateTime)
